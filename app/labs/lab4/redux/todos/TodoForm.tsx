dc314a5d418649de45d6d41dc6fca155ce6d2cfa
francespeters <peters.f@northeastern.edu>
2026-03-13 19:41:35 -0400

    a4 Redux part 1

---
+++ app/labs/lab4/redux/todos/TodoForm.tsx
@@ -0,0 +1,22 @@
+import { Button, FormControl, ListGroupItem } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, setTodo } from "./todosReducer";
+import { RootState } from "../../store";
+import { todo } from "node:test";
+
+export default function TodoForm(): {
+  todo: { id: string; title: string };
+  setTodo: (todo: { id: string; title: string }) => void;
+  addTodo: (todo: { id: string; title: string }) => void;
+  updateTodo: (todo: { id: string; title: string }) => void;
+}) {
+  return (
+    <ListGroupItem>
+      <Button onClick={() =>  dispatch(addTodo(todo))}
+              id="wd-add-todo-click"> Add </Button>
+      <Button onClick={() => dispatch(updateTodo(todo))}
+              id="wd-update-todo-click"> Update </Button>
+      <FormControl value={todo.title}
+        onChange={ (e) => dispatch(setTodo({ ...todo, title: e.target.value })) }/>
+    </ListGroupItem>
+);}
